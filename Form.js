a0f4eb601b17002b9eae4908eb86978eab8df7b8
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-04-17 10:26:55 +0200

    add style for form

--- Form.js
+++ Form.js
@@ -1,24 +1,40 @@
-
 export class Form {
 
     constructor(firstname, name, date, time, participants) {
-        this.firstname = firstname;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-        this.participants = participants;
+        this._firstname = firstname;
+        this._name = name;
+        this._date = date;
+        this._time = time;
+        this._participants = participants;
     }
 
     get firstname() {
-        return this.firstname
-    };
-
-    get name(){
-        return this.name;
+        return this._firstname;
     }
 
-    get participants(){
-        return this.name;
+    get name() {
+        return this._name;
     }
 
+    get date() {
+        return this._date;
+    }
+
+    get time() {
+        return this._time;
+    }
+
+    get participants() {
+        return this._participants;
+    }
+
+    toJSON() {
+        return {
+            firstname: this._firstname,
+            name: this._name,
+            date: this._date,
+            time: this._time,
+            participants: Number(this._participants)
+        };
+    }
 }
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const
+import { Form } from './Form.js';
+import { regexPatterns } from './regex.js';
+
+
+const bookingForm = document.getElementById("booking-form");
+const nameInput = document.getElementById("name");
+const nameValidation = document.getElementById("name-validation");
+const submitBtn = document.getElementById("submit-btn");
+
+bookingForm.addEventListener("submit", (event) =>{
+    event.preventDefault()
+
+
+})
