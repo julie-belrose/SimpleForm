08c6b5c1eb5059d1c797558e91dafa8a874bac8c
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-04-17 10:26:26 +0200

    fix regex to index.js

--- regex.js
+++ regex.js
@@ -1,7 +1,5 @@
 
-const regexPatterns = {
+export const regexPatterns = {
     name : `^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$`,
     inputNameOrLastName : `^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$`
 };
-
-export default regexPatterns;
